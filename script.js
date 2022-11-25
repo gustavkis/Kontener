f91e311eb38046f3262686f51882637ce5e43897
gustavkis <gustavkis.bp@gmail.com>
2022-11-25 21:38:01 +0100

    a js kód megírása - 3 funkció meghatározása BMI-hez hasonlóan

---
+++ script.js
@@ -0,0 +1,23 @@
+/* Felhasználom a BMI órai tanultakat
+megtalal() függvény:
+- az azonosít függvény return-ját elmentem egy változóba- eredmény,
+- meghívok egy függványt, amivel kiírom a return/változót
+function kiiras(szoveg) függvény
+- a BMI órán tanultak alapján kiírom az eredményt
+
+function azonosit() függvény
+- beazonosítom a kiválasztott település/konténer árát
+
+
+mindent ellenőrzök a consolban
+*/
+
+function megtalal() {
+  eredmeny = azonosit();
+  kiiras(eredmeny);
+  console.log(eredmeny);
+}
+
+function kiiras(szoveg) {}
+
+function azonosit() {}
